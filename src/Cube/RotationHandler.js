afe41582c804cb6c4bdd7d1954c2b06c042a9c88
MichaelDjab <mich.dja@gmail.com>
2024-09-14 10:02:45 -0400

    improve rotation handler

--- src/Cube/RotationHandler.js
+++ src/Cube/RotationHandler.js
@@ -4,46 +4,63 @@ class RotationHandler{
     constructor (scene, rubiksCube){
         this.scene = scene;
         this.rubiksCube = rubiksCube;
+        this.ANGLES = {
+            clockwise : -Math.PI / 2,
+            counterClockwise: Math.PI / 2
+        }
+        this.INTERVAL_MS = 1;
+        this.ROTATION_FRACTION = 0.05;
+        this.ROTATION_AXES = {
+            x : new THREE.Vector3(1, 0, 0),
+            y : new THREE.Vector3(0, 1, 0),
+            z : new THREE.Vector3(0, 0, 1)
+        }
     }
 
     rotateWhiteFace(direction) {
         const subCubesToRotate = this.GetCubesOnWhiteFace();
-        const rotationAxis = new THREE.Vector3(0, 0, 1);
-        const angle = (direction === 'clockwise') ? -Math.PI / 2 : Math.PI / 2;
-        const rotationSpeed = 0.1;
+        const rotationAxis = this.ROTATION_AXES.z;
+        const angle = this.getRotationAngle(direction);
         let currentRotation = 0;
-
+    
         // Create a temporary group to rotate the cubes together
-        const group = new THREE.Group();
-        subCubesToRotate.forEach(subCube => group.add(subCube));
-        this.scene.add(group);
-
-        // Animation function for rotation
-        const animateRotation = () => {
-            if (!this.IsRotationComplete(currentRotation, angle)) {
-                const step = rotationSpeed * Math.sign(angle);
+        const temporaryGroup = new THREE.Group();
+        subCubesToRotate.forEach(subCube => temporaryGroup.add(subCube));
+        this.scene.add(temporaryGroup);
+    
+        const step = this.ROTATION_FRACTION * Math.sign(angle);
+        const totalSteps = Math.abs(angle / step);
+        let stepsCompleted = 0;
+    
+        const rotationInterval = setInterval(() => {
+            if (stepsCompleted < totalSteps) {
+                temporaryGroup.rotateOnAxis(rotationAxis, step);
                 currentRotation += step;
-                group.rotateOnAxis(rotationAxis, step);
-                requestAnimationFrame(animateRotation);
+                stepsCompleted++;
             } else {
-                completeRotation();
+                this.completeRotation(rotationInterval, temporaryGroup, rotationAxis, angle, currentRotation, subCubesToRotate);
             }
-        };
+        }, this.INTERVAL_MS); 
+    }    
 
-        // Finalize rotation and apply to cubes
-        const completeRotation = () => {
-            group.rotateOnAxis(rotationAxis, angle - currentRotation);
-            group.updateMatrixWorld(true);
+    getRotationAngle(direction) {
+        return (direction === 'clockwise') ?
+            this.ANGLES.clockwise :
+            this.ANGLES.counterClockwise;
+    }
 
-            subCubesToRotate.forEach(subCube => {
-                subCube.applyMatrix4(group.matrixWorld);
-                this.scene.add(subCube);
-            });
+    completeRotation(rotationInterval, group, rotationAxis, angle, currentRotation, subCubesToRotate) {
+        clearInterval(rotationInterval);
+        group.rotateOnAxis(rotationAxis, angle - currentRotation);
+        group.updateMatrixWorld(true);
 
-            this.scene.remove(group);
-        };
+        subCubesToRotate.forEach(subCube => {
+            subCube.applyMatrix4(group.matrixWorld);
+            this.scene.add(subCube);
+            group.remove(subCube);
+        });
 
-        animateRotation();
+        this.scene.remove(group);
     }
 
     IsRotationComplete(currentRotation, angle) {
